d146ff46bb6d282260cca6b54db76c096c4976ad
Rihito178 <240160@jc-21.jp>
2026-02-06 15:29:25 +0900

    ネットワーク部分追加後

--- AirHockey/ソース.cpp
+++ AirHockey/ソース.cpp
@@ -1,7 +1,13 @@
-#include"DxLib.h"
+#define WIN32_LEAN_AND_MEAN
 #include<math.h>
 #include<stdlib.h>
+#include <WinSock2.h>
 #include<windows.h>
+#include"DxLib.h"
+#include <WS2tcpip.h>
+
+#pragma comment( lib, "ws2_32.lib" )
+using namespace std;
 
 //ボールの跳ね返りの計算(クリア) 
 //ボールを自動で動かす(クリア)
@@ -20,7 +26,14 @@ struct CIRCLE
 	int id; //送信元クライアントID
 };
 
+const char* SERVER_IPADDRSS = "127.0.0.1";	//サーバーのIP番号
+const unsigned short SERVER_PORT = 8888;	//サーバーのポート番号
 
+static bool set_nonblocking(SOCKET s)
+{
+	unsigned long arg = 1;
+	return ioctlsocket(s, FIONBIO, &arg) != SOCKET_ERROR;
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -37,9 +50,58 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	const int promptFont = 30;//プロンプト文字サイズ
 	const int promptChars = 21;//プロンプト文字数
-	
+
 	CIRCLE myCircle = { 0, 0, 5, GetColor(0, 255, 0) }; //idは未使用(サーバ側が付与)
 	POINT cursorPos;//マウスカーソルの位置取得変数
+
+	// WinSock2.2 初期化処理
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		return -1;
+	}
+
+	// TCPリスンソケットの作成
+	SOCKET listenSock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSock == INVALID_SOCKET)
+	{
+		WSACleanup();
+		return -1;
+	}
+	set_nonblocking(listenSock);
+
+	SOCKADDR_IN serverSockAddress{};
+	serverSockAddress.sin_family = AF_INET;
+	serverSockAddress.sin_port = htons(SERVER_PORT);
+	serverSockAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(listenSock, (SOCKADDR*)&serverSockAddress, sizeof(serverSockAddress)) == SOCKET_ERROR)
+	{
+		closesocket(listenSock);
+		WSACleanup();
+		return 1;
+	}
+
+	if (listen(listenSock, SOMAXCONN) == SOCKET_ERROR)
+	{
+		closesocket(listenSock);
+		WSACleanup();
+		return 1;
+	}
+
+
+	// 2クライアント用のソケットを用意
+	SOCKET socks[2];
+
+	// 2クライアント用のソケットアドレス構造体を用意
+	SOCKADDR_IN clientSocketAddresses[2];
+	SOCKADDR_IN clientSockAddress{};
+	int clientAddressLength = sizeof(clientSockAddress);
+
+
+
+	// クライアントのソケットアドレス
+
+
 
 	SetWindowText("テニスゲーム");//ウインドウのタイトル
 
@@ -81,7 +143,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int topRacketH = 12;
 
 	//画面切り替え
-	enum { TITLE, CONNECT,PLAY, OVER };
+	enum { TITLE, CONNECT, PLAY, OVER };
 	int scene = TITLE;
 	int timer = 0;
 	int score = 0;//スコア入力
@@ -94,6 +156,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int P1PlayAreaX = WIDTH;//1Pのプレイエリア境界線
 	int P1PlayAreaY = HEIGHT / 2;//1Pのプレイエリア境界線
 
+	const int MAX_CLIENT = 2;	// クライアントの最大数
+	int clientCount = 0;	// 接続中のクライアント数の計測用
+	int startTime = 0;	// 接続完了後から開始までのカウント用
+
 
 
 
@@ -101,26 +167,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	while (1)//{}内を繰り返し行う
 	{
-	
 
-		
+
+
 		ClearDrawScreen();//描画クリア
 		timer++;
-		
-		
+
+
 
 		switch (scene) //画面の分岐
 		{
 		case TITLE://タイトル
 			SetFontSize(Tx_GTitleFont);
-			
-			
+
+
 			if (timer % 60 < 30)
 			{
 				SetFontSize(Tx_GSystemFont);
-				
+
 			}
-			
+
 			if (CheckHitKey(KEY_INPUT_SPACE))//スペースキーを押したとき開始する
 			{
 				ballX = 40;
@@ -130,23 +196,71 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 				racketX = WIDTH / 2;
 				racketY = HEIGHT - 50;
 				score = 0;
-				scene = PLAY;
+
+
+				scene = CONNECT;
 				PlaySoundMem(bgm, DX_PLAYTYPE_LOOP);//ループ再生
 			}
-		break;
+			break;
 
+		case CONNECT:
+			if (clientCount < MAX_CLIENT)
+			{
+				DrawString(WIDTH - 400, HEIGHT - 200, "接続待機中...", WHITE, TRUE);
+			}
+
+			for (int i = 0; i < MAX_CLIENT; i++)
+			{
+				int addressLength = sizeof(clientSocketAddresses[0]);
+				SOCKET tmpSock = accept(listenSock, (SOCKADDR*)&clientSocketAddresses[clientCount], &addressLength);
+				if (tmpSock == INVALID_SOCKET)
+				{
+					int err = WSAGetLastError();
+					if (err == WSAEWOULDBLOCK)
+					{
+						// クライアント未接続→そのまま待つ
+						break;
+					}
+					else
+					{
+						// 本当のエラー
+					}
+				}
+				else
+				{
+					socks[clientCount] = tmpSock;
+					char ipaddress[20];
+					inet_ntop(AF_INET, &clientSocketAddresses[clientCount].sin_addr, ipaddress, sizeof(ipaddress));
+					clientCount++;
+				}
+
+			}
+
+			if (clientCount >= MAX_CLIENT)
+			{
+				if (startTime == 0) startTime = GetNowCount();
+
+				int elapsed = GetNowCount() - startTime;
+				DrawFormatString(WIDTH - 20, HEIGHT - 10, WHITE, "ゲーム開始まで　%d　秒", 3 - elapsed / 1000);
+				if (elapsed >= 3000)
+				{
+					scene = PLAY;
+					startTime = 0;
+				}
+			}
+			break;
 
 		case PLAY://ゲームプレイ
 
 			//int bollの跳ね返りの計算
 			ballX = ballX + ballVx;//ボールの中心（Ｘ）＝ボールの中心（Ｘ）＋Ｘの速さ
 			if (ballX < ballR && ballVx < 0)ballVx = -ballVx;//ボールのX座標＜ボールの半径になる上X座標の速さ＜０の場合
-			
+
 			if (ballX > WIDTH - ballR && ballVx > 0)ballVx = -ballVx;//ボールのX座標＞横幅になる上X座標の速さ＞０の場合
 
 			ballY = ballY + ballVy;//ボールの中心（Ｙ）＝ボールの中心（Ｙ）＋Ｙの速さ
 			if (ballY < ballR && ballVy < 0)ballVy = -ballVy;//ボールのY座標＜ボールの半径になる上Y座標の速さ＜０の場合
-			
+
 			//if (ballY > HEIGHT && ballVy > 0)ballVy = -ballVy;//ボールのY座標＜ボールの高さになる上Y座標の速さ＜０の場合
 
 			if (ballY > HEIGHT)//ボールが下の端に到達したとき終了する
@@ -160,34 +274,34 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 
 			//ラケット
-		
-
-				if (GetCursorPos(&cursorPos))
-				{
-
-					//マウス位置を取得して更新
-					GetMousePoint(&myCircle.centerX, &myCircle.centerY);
-					if (myCircle.centerY < P1PlayAreaY) myCircle.centerY = P1PlayAreaY;//1Pのプレイエリア制限
-
-					if (racketX < racketW / 2)racketX = racketW / 2;//ラケットの左端制限
-					//円の描画
-					DrawCircle(myCircle.centerX, myCircle.centerY, myCircle.radius, myCircle.color, TRUE);
-					
-					
-					DrawBox(
-						racketX - racketW / 2,
-						racketY - racketH / 2,
-						racketX + racketW / 2,
-						racketY + racketH / 2,
-						0x0080ff, TRUE);
-
-					racketX = myCircle.centerX;//ラケットの中心(X)
-					racketY = myCircle.centerY;//ラケットの中心(Y)
 
 
-				}
+			if (GetCursorPos(&cursorPos))
+			{
 
-				
+				//マウス位置を取得して更新
+				GetMousePoint(&myCircle.centerX, &myCircle.centerY);
+				if (myCircle.centerY < P1PlayAreaY) myCircle.centerY = P1PlayAreaY;//1Pのプレイエリア制限
+
+				if (racketX < racketW / 2)racketX = racketW / 2;//ラケットの左端制限
+				//円の描画
+				DrawCircle(myCircle.centerX, myCircle.centerY, myCircle.radius, myCircle.color, TRUE);
+
+
+				DrawBox(
+					racketX - racketW / 2,
+					racketY - racketH / 2,
+					racketX + racketW / 2,
+					racketY + racketH / 2,
+					0x0080ff, TRUE);
+
+				racketX = myCircle.centerX;//ラケットの中心(X)
+				racketY = myCircle.centerY;//ラケットの中心(Y)
+
+
+			}
+
+
 
 
 
@@ -205,8 +319,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 				if (topRacketX > WIDTH - topRacketW / 2)topRacketX = WIDTH - topRacketW / 2;
 			}
 
-	
-			
+
+
 
 			DrawBox(
 				topRacketX - topRacketW / 2,
